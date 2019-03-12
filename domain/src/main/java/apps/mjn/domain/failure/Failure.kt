15e920844b9d197ce748bdd75be946077cb99d9a
mJafarinejad <mjn1369@gmail.com>
2019-03-12 09:50:56 +0330

    added sealed class "Failure" to cover different kinds of failures

---
+++ domain/src/main/java/apps/mjn/domain/failure/Failure.kt
@@ -0,0 +1,8 @@
+package apps.mjn.domain.failure
+
+sealed class Failure(override val message: String) : Throwable(message) {
+    class NetworkConnectionError(message: String) : Failure(message)
+    class ServerError(message: String) : Failure(message)
+    class NotFoundError(message: String) : Failure(message)
+    class DisabledError(message: String) : Failure(message)
+}
