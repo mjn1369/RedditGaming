a13e428c3531af25babd9d1628a8196ea3750708
mJafarinejad <mjn1369@gmail.com>
2019-03-12 14:20:43 +0330

    updated splash view model to grab list from use case and store in live data

--- app/src/main/java/apps/mjn/redditgaming/ui/splash/SplashViewModel.kt
+++ app/src/main/java/apps/mjn/redditgaming/ui/splash/SplashViewModel.kt
@@ -1,13 +1,35 @@
 package apps.mjn.redditgaming.ui.splash
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import apps.mjn.domain.entity.RedditPostList
 import apps.mjn.domain.interactor.GetRedditListUseCase
 import apps.mjn.redditgaming.ui.base.BaseViewModel
+import apps.mjn.redditgaming.ui.model.Resource
+import apps.mjn.redditgaming.ui.model.ResourceState
 import javax.inject.Inject
 
 class SplashViewModel @Inject constructor(private val redditListUseCase: GetRedditListUseCase) :
     BaseViewModel() {
 
+    private val data: MutableLiveData<Resource<RedditPostList>> = MutableLiveData()
+
     init {
         useCases += redditListUseCase
+    }
+
+    fun getData(): LiveData<Resource<RedditPostList>> = data
+
+    fun load(nextPageTag: String) {
+        data.value = Resource(ResourceState.LOADING)
+        redditListUseCase.execute(GetRedditListUseCase.Params(nextPageTag), ::success, ::error)
+    }
+
+    private fun success(list: RedditPostList) {
+        data.value = Resource(ResourceState.SUCCESS, list)
+    }
+
+    private fun error(throwable: Throwable) {
+        data.value = Resource(ResourceState.ERROR, failure = throwable)
     }
 }
